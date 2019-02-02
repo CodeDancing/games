4df0db4f7aef49cd2b26b39a20e9e1146f12e478
andy.lq <andy.lq@koubei.com>
2019-02-02 15:36:06 +0800

    express框架写snake

---
+++ testWebApp/routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* GET game snake. */
+router.get('/game', function(req, res, next) {
+  res.render('game');
+});
+module.exports = router;
